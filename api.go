63c3561351a285e23288fb976135b808e9e643d0
kenshin1025 <sukoyakamind1025@gmail.com>
2020-05-07 03:04:03 +0900

    STEP4までの必要要件は満たせたはず、ただ同じ名前で登録するとtokenがかぶる

--- api.go
+++ api.go
@@ -7,33 +7,88 @@ import (
 	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
 	"io"
+	"log"
 	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
+// dbを全ての関数で使いたいので宣言
 var db *sql.DB
 
+// user情報のjson用の構造体
 type userJSON struct {
 	Name string `json:"name"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world\n")
+// userのtokenのjson用の構造体
+type tokenJSON struct {
+	Token string `json:"token"`
 }
 
+// Token 作成関数
+func createToken(user userJSON) (string, error) {
+	var err error
+
+	// 鍵となる文字列(多分なんでもいい)
+	secret := "secret"
+
+	// Token を作成
+	// jwt -> JSON Web Token - JSON をセキュアにやり取りするための仕様
+	// jwtの構造 -> {Base64 encoded Header}.{Base64 encoded Payload}.{Signature}
+	// HS254 -> 証明生成用(https://ja.wikipedia.org/wiki/JSON_Web_Token)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"name": user.Name,
+		"iss":   "__init__", // JWT の発行者が入る(文字列(__init__)は任意)
+	})
+
+	tokenString, err := token.SignedString([]byte(secret))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return tokenString, nil
+}
+
+// ユーザー作成
 func create(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
+		// リクエストbody(json)を受け取る
 		body := r.Body
 		defer body.Close()
 
+		// byte配列に変換するためにcopy
 		buf := new(bytes.Buffer)
 		io.Copy(buf, body)
 
+		// byte配列にしたbody内のjsonをgoで扱えるようにobjectに変換
 		var user userJSON
 		json.Unmarshal(buf.Bytes(), &user)
-		
-		w.WriteHeader(http.StatusCreated)
-		fmt.Fprintf(w, "POST hello! %v\n", user)
+
+		// jwtでtoken作成
+		token, err := createToken(user)
+
+		// DBに追加
+		//レコードを取得する必要のない、クエリはExecメソッドを使う。
+		_, err = db.Exec("INSERT INTO user(name, token) VALUES(?,?)", user.Name, token)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// レスポンス用のjson生成
+		t := tokenJSON{token}
+		res, err := json.Marshal(t)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// レスポンス
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+
 	default:
 		fmt.Fprint(w, "Method not allowed.\n")
 	}
@@ -42,16 +97,25 @@ func create(w http.ResponseWriter, r *http.Request) {
 func get(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		rows, err := db.Query("SELECT * FROM user") //
-		if err != nil {
-			panic(err.Error())
+		// リクエストheaderを受け取る
+		header := r.Header
+
+		// tokenを元にユーザーのnameを取得
+		var name string
+		if err := db.QueryRow("SELECT name FROM user WHERE token = ?", header.Get("x-token")).Scan(&name); err != nil {
+			log.Fatal(err)
 		}
 
-		columns, err := rows.Columns() // カラム名を取得
+		// レスポンス用のjson生成
+		u := userJSON{name}
+		res, err := json.Marshal(u)
 		if err != nil {
-			panic(err.Error())
+			log.Fatal(err)
 		}
-		fmt.Fprintf(w, "Hello get%v\n", columns)
+
+		// レスポンス
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
 	default:
 		fmt.Fprint(w, "Method not allowed.\n")
 	}
@@ -60,7 +124,27 @@ func get(w http.ResponseWriter, r *http.Request) {
 func update(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "PUT":
-		fmt.Fprintf(w, "Hello put\n")
+		// リクエストを受け取る
+		header := r.Header
+		body := r.Body
+		defer body.Close()
+
+		// byteに変換するためにcopy
+		buf := new(bytes.Buffer)
+		io.Copy(buf, body)
+
+		// byte配列にしたbody内のjsonをgoで扱えるようにobjectに変換
+		var user userJSON
+		json.Unmarshal(buf.Bytes(), &user)
+
+		// tokenを元にユーザーのnameを更新
+		_, err := db.Exec("UPDATE user SET name = ? WHERE token = ?", user.Name, header.Get("x-token"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// レスポンス
+		w.WriteHeader(http.StatusOK)
 	default:
 		fmt.Fprint(w, "Method not allowed.\n")
 	}
@@ -75,7 +159,6 @@ func main() {
 	}
 	defer db.Close()
 
-	http.HandleFunc("/", handler)
 	http.HandleFunc("/user/create", create)
 	http.HandleFunc("/user/get", get)
 	http.HandleFunc("/user/update", update)
