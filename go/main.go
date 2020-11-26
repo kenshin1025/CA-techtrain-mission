b0c6f5d72b8d99655cf8d2996feabd2582fbc264
kenshin1025 <sukoyakamind1025@gmail.com>
2020-11-26 09:26:58 +0900

    dockerでmysqlを起動し、init.sqlで初期化を行い、接続するところまで確認できた。

--- go/main.go
+++ go/main.go
@@ -215,7 +215,7 @@ func main() {
 	}
 
 	//.envファイルからdataSourceNameを作成してDBに接続する
-	dsn := fmt.Sprintf("%s:%s@tcp(db:3306)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_DATABASE"))
+	dsn := fmt.Sprintf("%s:%s@tcp(db:3306)/%s", os.Getenv("DB_ROOT_USER"), os.Getenv("DB_ROOT_PASS"), os.Getenv("DB_DATABASE"))
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
