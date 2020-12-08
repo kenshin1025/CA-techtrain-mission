a86acec8452a683cf1355388a44f180c58a88bf5
kenshin1025 <sukoyakamind1025@gmail.com>
2020-12-08 21:53:42 +0900

    入らなそうなので削除(こんなんで良いのか？)

--- db/init/1_init.sql
+++ db/init/1_init.sql
@@ -8,18 +8,18 @@ CREATE TABLE IF NOT EXISTS user (
   token VARCHAR(100) NOT NULL,
   created_at timestamp NOT NULL default current_timestamp,
   updated_at timestamp NOT NULL default current_timestamp on update current_timestamp
-) DEFAULT CHARSET = utf8;
+);
 
 CREATE TABLE IF NOT EXISTS chara (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL
-) DEFAULT CHARSET = utf8;
+);
 
 CREATE TABLE IF NOT EXISTS rarity (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   probability int NOT NULL
-) DEFAULT CHARSET = utf8;
+);
 
 CREATE TABLE IF NOT EXISTS chara_rarity (
   chara_id int,
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS chara_rarity (
   PRIMARY KEY(chara_id, rarity_id),
   FOREIGN KEY (chara_id) REFERENCES chara(id) ON DELETE CASCADE,
   FOREIGN KEY (rarity_id) REFERENCES rarity(id) ON DELETE CASCADE
-) DEFAULT CHARSET = utf8;
+);
 
 INSERT INTO
   user (name, token)
