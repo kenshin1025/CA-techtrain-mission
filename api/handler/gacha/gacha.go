00989bba3435bdf30b4b2ecd85610226e2f6d478
kenshin1025 <sukoyakamind1025@gmail.com>
2020-12-11 23:02:25 +0900

    dbを参照してそこから1回ガチャが引けるようになった（文字化けあり）

--- api/handler/gacha/gacha.go
+++ api/handler/gacha/gacha.go
@@ -4,21 +4,73 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func Draw(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	character := oneDraw(0.01)
-	fmt.Fprint(w, character)
+type Rarity struct {
+	probability float64
+	charas      []Character
 }
 
-func oneDraw(n float64) string {
-	chara := ""
-	if n < 0.01 {
-		chara = "大当たり"
-	} else if n < 0.11 {
-		chara = "当たり"
-	} else {
-		chara = "ハズレ"
+type Character struct {
+	id   string
+	name string
+}
+
+func Draw(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	character, err := oneDraw(0.01, db)
+	if err != nil {
+		fmt.Fprint(w, "エラーやんけ！")
+		return
 	}
-	return chara
+	fmt.Println(character.name)
+	fmt.Fprint(w, character.name)
+}
+
+func oneDraw(n float64, db *sql.DB) (*Character, error) {
+	config, err := getConfig(db)
+	if err != nil {
+		return nil, err
+	}
+	boundary := 0.0
+	for _, raritiy := range config {
+		for _, chara := range raritiy.charas {
+			boundary += raritiy.probability / float64(len(raritiy.charas))
+			if n <= boundary {
+				return &chara, nil
+			}
+		}
+	}
+	return nil, err
+}
+
+func getConfig(db *sql.DB) ([]Rarity, error) {
+	var config []Rarity
+	rows, err := db.Query("SELECT rarity.probability, GROUP_CONCAT(chara.id), GROUP_CONCAT(chara.name) FROM rarity JOIN chara ON rarity.id = chara.rarity_id GROUP BY rarity.id ORDER BY rarity.probability")
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var r Rarity
+		var stringCharaIDs string
+		var stringCharaNames string
+		if err := rows.Scan(&r.probability, &stringCharaIDs, &stringCharaNames); err != nil {
+			return nil, err
+		}
+
+		sliceCharaIDs := strings.Split(stringCharaIDs, ",")
+		sliceCharaNames := strings.Split(stringCharaNames, ",")
+		var charas []Character
+		for i, CharaID := range sliceCharaIDs {
+			charas = append(charas, Character{id: CharaID, name: sliceCharaNames[i]})
+		}
+		r.charas = charas
+		config = append(config, r)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	rows.Close()
+
+	return config, nil
 }
--- db/init.d/1_init.sql
+++ db/init.d/1_init.sql
@@ -10,26 +10,40 @@ CREATE TABLE IF NOT EXISTS user (
   updated_at timestamp NOT NULL default current_timestamp on update current_timestamp
 );
 
+CREATE TABLE IF NOT EXISTS rarity (
+  id int NOT NULL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  probability float NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS chara (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS rarity (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  probability int NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS chara_rarity (
-  chara_id int,
   rarity_id int,
-  PRIMARY KEY(chara_id, rarity_id),
-  FOREIGN KEY (chara_id) REFERENCES chara(id) ON DELETE CASCADE,
   FOREIGN KEY (rarity_id) REFERENCES rarity(id) ON DELETE CASCADE
 );
 
 INSERT INTO
-  user (name, token)
+  rarity
 VALUES
-  ("kenshin", "help");
+  (1, "SSR", 0.01),
+  (2, "SR", 0.1),
+  (3, "R", 0.89);
+
+INSERT INTO
+  chara (name, rarity_id)
+VALUES
+  ("めちゃめちゃレア1", 1),
+  ("めちゃめちゃレア２", 1),
+  ("めちゃレア１", 2),
+  ("めちゃレア2", 2),
+  ("めちゃレア3", 2),
+  ("めちゃレア4", 2),
+  ("レア1", 3),
+  ("レア2", 3),
+  ("レア3", 3),
+  ("レア4", 3),
+  ("レア5", 3),
+  ("レア6", 3),
+  ("レア7", 3),
+  ("レア8", 3);
