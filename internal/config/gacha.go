893fb1fb0879cd6fb70fcf4a599a84b2c40b8589
kenshin1025 <sukoyakamind1025@gmail.com>
2021-03-16 01:04:44 +0900

    ガチャの設定を生成する用の関数を実装した

---
+++ internal/config/gacha.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"ca-mission/internal/model"
+	"database/sql"
+)
+
+type GachaConfig struct {
+	SumAllProbability int
+	Charas            []*model.Chara
+}
+
+func GenerateGachaConfig(db *sql.DB) (*GachaConfig, error) {
+	var g GachaConfig
+	rows, err := db.Query("SELECT * FROM chara")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var c model.Chara
+		if err := rows.Scan(&c.ID, &c.Name, &c.Probability); err != nil {
+			return nil, err
+		}
+		g.SumAllProbability += c.Probability
+		g.Charas = append(g.Charas, &c)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return &g, nil
+}
