14eb0038ff862cfd0228449efeb742a64d0bf38e
kenshin1025 <sukoyakamind1025@gmail.com>
2021-03-15 14:29:27 +0900

    getリクエストのテスト追加

--- e2e/user_handler_test.go
+++ e2e/user_handler_test.go
@@ -18,7 +18,8 @@ import (
 
 // testで作成するuserのdata
 const (
-	name = "test_name"
+	name  = "test_name"
+	token = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaaaaaa"
 )
 
 func Test_E2E_CreateUser(t *testing.T) {
@@ -67,5 +68,48 @@ func Test_E2E_CreateUser(t *testing.T) {
 	}
 	if actual != name {
 		t.Errorf("name must be %s but %s", name, actual)
+	}
+}
+
+func Test_E2E_GetUser(t *testing.T) {
+	//DBに接続する
+	db, err := sql.Open("mysql", config.Config().GenerateDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		// DBのcleanを行う
+		db.Exec("set foreign_key_checks = 0")
+		db.Exec("truncate table user")
+		db.Exec("set foreign_key_checks = 1")
+		db.Close()
+	}()
+
+	if _, err := db.Exec("insert into user(name, token) values (?, ?)", name, token); err != nil {
+		t.Fatal(err)
+	}
+
+	userUsecase := usecase.NewUser(repository.NewUser(), db)
+
+	// requestをシュミレートする
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set("x-token", token)
+	rec := httptest.NewRecorder()
+	http.HandlerFunc(handler.GetUser(userUsecase)).ServeHTTP(rec, req)
+
+	// responseのStatus Codeをチェックする
+	if rec.Code != http.StatusOK {
+		t.Errorf("status code must be 200 but: %d", rec.Code)
+		t.Fatalf("body: %s", rec.Body.String())
+	}
+
+	var result handler.ResGetUserJSON
+	if err := json.NewDecoder(rec.Body).Decode(&result); err != nil {
+		t.Fatal(err)
+	}
+
+	// responseで返ってきたnameがinsertしたtokenと紐づいたnameか確認
+	if result.Name != name {
+		t.Errorf("name must be %s but %s", name, result.Name)
 	}
 }
