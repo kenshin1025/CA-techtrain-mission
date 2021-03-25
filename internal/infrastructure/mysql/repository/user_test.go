db1fe714c924485aab5919bb2641135e5f26a021
kenshin1025 <sukoyakamind1025@gmail.com>
2021-03-25 23:23:49 +0900

    インフラ層のクリエイト関数のテストを書いた

---
+++ internal/infrastructure/mysql/repository/user_test.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"ca-mission/internal/config"
+	"ca-mission/internal/domain/model"
+	"database/sql"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestCreate(t *testing.T) {
+	//DBに接続する
+	db, err := sql.Open("mysql", config.Config().GenerateDSN())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		// DBのcleanを行う
+		db.Exec("set foreign_key_checks = 0")
+		db.Exec("truncate table user")
+		db.Exec("set foreign_key_checks = 1")
+		db.Close()
+	}()
+
+	u := model.User{
+		Name:  "test_test",
+		Token: "test_token",
+	}
+
+	user := NewUser(db)
+
+	err = user.Create(&u)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var actual string
+	if err := db.QueryRow("select name from user where token = ?", u.Token).Scan(&actual); err != nil {
+		t.Fatal(err)
+	}
+	if actual != u.Name {
+		t.Errorf("name must be %s but %s", u.Name, actual)
+	}
+}
