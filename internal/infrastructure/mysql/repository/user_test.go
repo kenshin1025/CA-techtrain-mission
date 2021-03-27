e8cd883ff39ab3899fda8308dee6faa42ab14c9b
kenshin1025 <sukoyakamind1025@gmail.com>
2021-03-27 21:50:00 +0900

    getとupdateを実装した

--- internal/infrastructure/mysql/repository/user_test.go
+++ internal/infrastructure/mysql/repository/user_test.go
@@ -61,29 +61,24 @@ func TestGet(t *testing.T) {
 		db.Close()
 	}()
 
-	u := model.User{
-		Name:  name,
-		Token: token,
-	}
-
-	_, err = db.Exec("INSERT INTO user(name, token) VALUES(?,?)", u.Name, u.Token)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	u = model.User{
-		Token: token,
-	}
-
 	user := NewUser(db)
 
-	err = user.Get(&u)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if name != u.Name {
-		t.Errorf("name must be %s but %s", u.Name, name)
+	for i, u := range userTests {
+		t.Run(strconv.Itoa(i)+u.Name, func(t *testing.T) {
+			_, err = db.Exec("INSERT INTO user(name, token) VALUES(?,?)", u.Name, u.Token)
+			if err != nil {
+				t.Fatal(err)
+			}
+			insertName := u.Name
+			u.Name = ""
+			err := user.Get(u)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if insertName != u.Name {
+				t.Errorf("name must be %s but %s", u.Name, insertName)
+			}
+		})
 	}
 }
 
@@ -101,33 +96,26 @@ func TestUpdate(t *testing.T) {
 		db.Close()
 	}()
 
-	u := model.User{
-		Name:  name,
-		Token: token,
-	}
-
-	_, err = db.Exec("INSERT INTO user(name, token) VALUES(?,?)", u.Name, u.Token)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	u = model.User{
-		Name:  updateName,
-		Token: token,
-	}
-
 	user := NewUser(db)
 
-	err = user.Update(&u)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for i, u := range userTests {
+		t.Run(strconv.Itoa(i)+u.Name, func(t *testing.T) {
+			_, err = db.Exec("INSERT INTO user(name, token) VALUES(?,?)", "not_update_name", u.Token)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	var actual string
-	if err := db.QueryRow("select name from user where token = ?", token).Scan(&actual); err != nil {
-		t.Fatal(err)
-	}
-	if actual != updateName {
-		t.Errorf("name must be %s but %s", updateName, actual)
+			err := user.Update(u)
+			if err != nil {
+				t.Fatal(err)
+			}
+			var actual string
+			if err := db.QueryRow("select name from user where token = ?", u.Token).Scan(&actual); err != nil {
+				t.Fatal(err)
+			}
+			if actual != u.Name {
+				t.Errorf("name must be %s but %s", u.Name, actual)
+			}
+		})
 	}
 }
